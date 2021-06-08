9c037090af1f286c7ccb05ec56aad22bf6f1b935
revido <revidoberliano@gmail.com>
2021-06-08 20:26:21 +0700

    user & admin login

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends AdminController
+{
+    public function index()
+    {
+        $pageConfigs = ['pageHeader' => false];
+        return view('/admin/dashboard', ['pageConfigs' => $pageConfigs]);
+    }
+}
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,20 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/');
+
+        switch ($guard) {
+            case 'admin':
+                if (Auth::guard($guard)->check()) {
+                    return redirect('/admin');
+                }
+                break;
+
+                return $next($request);
+            default:
+                if (Auth::guard($guard)->check()) {
+                    return redirect('/');
+                }
+                break;
         }
 
         return $next($request);
