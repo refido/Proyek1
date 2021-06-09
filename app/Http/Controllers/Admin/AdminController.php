3be187442be9a7358bd462f9bbaf523d27fbd53f
Muhammad Daffa' A.R <daffavcd@gmail.com>
2021-06-09 09:43:21 +0700

    add middleware admin auth

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:admin');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
 }
